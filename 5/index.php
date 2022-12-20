1894d8419c06e893add47346524f1e78d3707a30
TiestoTool <112871170+TiestoTool@users.noreply.github.com>
2022-12-20 12:54:41 +0100

    cccc

---
+++ 5/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+        <input type="text" placeholder = " voornaam"> <br> <input type="text" placeholder = "achternaam">
+        <br>
+        <br>
+
+    <!-- man -->
+    <div>
+        <input type="radio" id = "man">
+        <label for="man"> man</label>
+    </div>
+    <!-- vrouw -->
+    <div>
+        <input type="radio" id = "vrouw">
+        <label for="vrouw">vrouw</label>
+    </div>
+    <input type="text" placeholder = "straat"> <br> <input type="number" placeholder = "huisnummer">
+
+</body>
+</html>
